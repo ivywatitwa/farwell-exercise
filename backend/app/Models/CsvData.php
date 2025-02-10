50768e16103306ab09e8804f53cfbc47cce3ac90
Ivy Watitwa <watitwaivy@gmail.com>
2025-02-10 21:42:23 +0300

    csv model

--- backend/app/Models/CsvData.php
+++ backend/app/Models/CsvData.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CsvData extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'file_path'
+    ];
 }
